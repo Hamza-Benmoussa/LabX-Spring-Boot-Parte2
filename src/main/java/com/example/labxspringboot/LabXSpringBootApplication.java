c2afc3ee38ad4cee632afc37aa259e4a5ff68fc2
hamza <hamzabenmoussa166@gmail.com>
2024-01-23 12:55:35 +0100

    add annotation @EnableSwagger2 and @EnableWebMvc

--- src/main/java/com/example/labxspringboot/LabXSpringBootApplication.java
+++ src/main/java/com/example/labxspringboot/LabXSpringBootApplication.java
@@ -2,8 +2,12 @@ package com.example.labxspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
 public class LabXSpringBootApplication {
 
     public static void main(String[] args) {
